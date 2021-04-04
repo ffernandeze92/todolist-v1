a7dd61fccd7334d89a9dabf6b5be77bcf49f367f
Fernando.F <fernan9214@gmail.com>
2021-04-04 13:27:00 -0500

    coommit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set('view engine','ejs');
+
 app.get("/",function(req,res){
   var today = new Date();
   var currentDay = today.getDay();
